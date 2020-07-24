d8e81f48781c6c48474b2d4dcdd51478f4fc10eb
Verch123 <verchsvaag.123@gmail.com>
2020-07-25 01:01:22 +0530

    - Third Commit

---
+++ app/src/main/java/com/example/splashscreenwithlogin/services/MyFirebaseInstanceService.java
@@ -0,0 +1,104 @@
+package com.example.splashscreenwithlogin.services;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.example.splashscreenwithlogin.Homepage;
+import com.example.splashscreenwithlogin.R;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+import java.util.Random;
+
+public class MyFirebaseInstanceService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage){
+        super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getData().isEmpty()){
+            showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+        }
+        else {
+            showNotification(remoteMessage.getData());
+        }
+    }
+
+    private void showNotification(Map<String,String>data){
+        String title=data.get("title").toString();
+        String body=data.get("body").toString();
+
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID="example.firbase.services.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel=new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Code Sphere");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        Intent resultIntent=new Intent(this, Homepage.class);
+        PendingIntent reultPendingIntent=PendingIntent.getActivity(this,1,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationBuilder.setAutoCancel(true)
+                   .setDefaults(Notification.DEFAULT_ALL)
+                   .setWhen(System.currentTimeMillis())
+                   .setSmallIcon(R.drawable.c_l)
+                .setContentIntent(reultPendingIntent)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+    }
+    private void showNotification(String title,String body){
+
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID="example.firbase.services.test";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel=new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Code Sphere");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        Intent resultIntent=new Intent(this, Homepage.class);
+        PendingIntent reultPendingIntent=PendingIntent.getActivity(this,1,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.c_l)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info")
+                .setContentIntent(reultPendingIntent);
+
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s){
+        super.onNewToken(s);
+
+        Log.d("TOKENFIREBASE",s);
+    }
+}
